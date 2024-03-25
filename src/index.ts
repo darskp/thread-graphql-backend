454cea90e3cb6b7f33e36027e88fcbc97798c421
darskp <darshanpatgar100@gmail.com>
2024-03-25 10:51:41 +0530

    apollo server setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 import express from "express";
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 
-const app = express();
-const PORT = Number(process.env.PORT) || 8000
+async function init() {
+    const app = express();
+    const PORT = Number(process.env.PORT) || 8000;
+    app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({ messsage: "Fetched Successfully" })
-})
+    const gqlserver = new ApolloServer({
+        typeDefs: `
+        type Query{
+        hello:String
+        }
+        `,
+        resolvers: {
+            Query:{
+                hello:()=>"Hi"
+            } 
+        }
+    })
+    await gqlserver.start();
+    app.use('/graphql', expressMiddleware(gqlserver))
 
-app.listen(PORT, 'localhost', () => {
-    console.log(`listening at the server - http://localhost:${PORT}`);
-})
+    app.get('/', (req, res) => {
+        res.json({ messsage: "Fetched Successfully" })
+    })
+
+    app.listen(PORT, 'localhost', () => {
+        console.log(`listening at the server - http://localhost:${PORT}`);
+    })
+}
+init();
