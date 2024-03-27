216c5c307dca2a383eb3bb6a1b8655ac68a17778
darskp <darshanpatgar100@gmail.com>
2024-03-27 09:47:29 +0530

    crud mutation

--- build/index.js
+++ build/index.js
@@ -15,20 +15,66 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const server_1 = require("@apollo/server");
 const express4_1 = require("@apollo/server/express4");
+const body_parser_1 = __importDefault(require("body-parser"));
+const axios_1 = __importDefault(require("axios"));
+const JSON_API = `http://localhost:5000/user`;
 function init() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = (0, express_1.default)();
         const PORT = Number(process.env.PORT) || 8000;
-        app.use(express_1.default.json());
+        app.use(body_parser_1.default.json());
         const gqlserver = new server_1.ApolloServer({
             typeDefs: `
+
+        type User{
+        id:ID!,
+        Name:String!,
+        email:String!
+        }
+
         type Query{
         hello:String
+        say(name:String):String
+        getUser:[User]
+        }
+
+        type Mutation {
+            addUser(user: inputUser!): User
+            deleteUser(id: ID!): User
+            updateUser(id: ID!, edit: inputUser!): User
+        }
+
+        input inputUser{
+        Name:String!,
+        email:String!
         }
         `,
             resolvers: {
                 Query: {
-                    hello: () => "Hi"
+                    hello: () => "Hi",
+                    say: (parent, args) => `Hey! ${args.name} How are you?`,
+                    getUser: () => __awaiter(this, void 0, void 0, function* () { return (yield axios_1.default.get(`${JSON_API}`)).data; }),
+                },
+                Mutation: {
+                    addUser(parent, args) {
+                        return __awaiter(this, void 0, void 0, function* () {
+                            let newUserData = Object.assign(Object.assign({}, args.user), { id: Math.floor(Math.random() * 1000).toString() });
+                            let response = yield axios_1.default.post(`${JSON_API}`, newUserData);
+                            return response.data;
+                        });
+                    },
+                    deleteUser(parent, args) {
+                        return __awaiter(this, void 0, void 0, function* () {
+                            let response = yield axios_1.default.delete(`${JSON_API}/${args.id}`);
+                            return response.data;
+                        });
+                    },
+                    updateUser(parent, args) {
+                        return __awaiter(this, void 0, void 0, function* () {
+                            let response = yield axios_1.default.(`${JSON_API}/${args.id}`, args.edit);
+                            return response.data;
+                        });
+                    }
                 }
             }
         });
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,71 @@
 import express from "express";
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser, { json } from "body-parser";
+import axios from "axios";
+
+const JSON_API = `http://localhost:5000/user`;
 
 async function init() {
     const app = express();
     const PORT = Number(process.env.PORT) || 8000;
-    app.use(express.json())
+    app.use(bodyParser.json())
 
     const gqlserver = new ApolloServer({
         typeDefs: `
+
+        type User{
+        id:ID!,
+        Name:String!,
+        email:String!
+        }
+
         type Query{
         hello:String
+        say(name:String):String
+        getUser:[User]
+        }
+
+        type Mutation {
+            addUser(user: inputUser!): User
+            deleteUser(id: ID!): User
+            updateUser(id: ID!, edit: inputUser!): User
+        }
+
+        input inputUser{
+        Name:String!,
+        email:String!
         }
         `,
         resolvers: {
-            Query:{
-                hello:()=>"Hi"
-            } 
+            Query: {
+                hello: () => "Hi",
+                say: (parent, args: { name: String }) =>
+                    `Hey! ${args.name} How are you?`,
+                getUser: async () => (await axios.get(`${JSON_API}`)).data,
+            },
+            Mutation: {
+                async addUser(parent, args) {
+                    let newUserData = {
+                        ...args.user,
+                        id: Math.floor(Math.random() * 1000).toString()
+                    }
+                    let response = await axios.post(`${JSON_API}`, newUserData);
+                    return response.data;
+                },
+                async deleteUser(parent, args) {
+                    let response = await axios.delete(`${JSON_API}/${args.id}`);
+                    return response.data
+                },
+                async updateUser(parent, args) {
+                    let response = await axios.put(`${JSON_API}/${args.id}`,args.edit);
+                    return response.data
+                }
+            }
         }
     })
     await gqlserver.start();
     app.use('/graphql', expressMiddleware(gqlserver))
-
     app.get('/', (req, res) => {
         res.json({ messsage: "Fetched Successfully" })
     })
